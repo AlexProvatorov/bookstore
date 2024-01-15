649cd9bdde3b08c19d566e55da5e0f2f413e3e7e
AlexProvatorov <sasha.provatorov@gmail.com>
2024-01-15 15:45:38 +0300

    make first model and  migration

--- bookstore/book_store/models.py
+++ bookstore/book_store/models.py
@@ -1,3 +1,55 @@
 from django.db import models
 
-# Create your models here.
+
+class Items(models.Model):
+    id = models.AutoField(primary_key=True)
+    type = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    cost = models.IntegerField()
+    count_in_stock = models.IntegerField()
+    tags = models.ManyToManyField('Tags', blank=True, related_name='tags')
+    id_cart = models.ForeignKey(
+        'Cart',
+        on_delete=models.PROTECT,
+    )
+
+
+class Tags(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+
+class Customers(models.Model):
+    id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField()
+    date_of_birth = models.DateField()
+    items = models.ManyToManyField('Items', blank=True, related_name='items')
+
+
+class Cart(models.Model):
+    id_customer = models.OneToOneField(
+        'Customers',
+        on_delete=models.PROTECT,
+    )
+    count = models.IntegerField()
+
+
+class Books(Items):
+    authors = models.ManyToManyField('Authors', related_name='authors')
+    date_release = models.DateField()
+
+
+class Authors(models.Model):
+    id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+
+class Figurine(Items):
+    weight = models.FloatField()
+    height = models.FloatField()
+    width = models.FloatField()
