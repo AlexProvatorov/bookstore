856d06ed68e50440b3f06275152da235d1824431
AlexProvatorov <sasha.provatorov@gmail.com>
2024-01-17 15:55:54 +0300

    feat: create templates: index, catalog, cart

--- bookstore/book_store/urls.py
+++ bookstore/book_store/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.index),
     path('catalog/', views.catalog),
     path('catalog/<slug:slug>', views.catalog_by_slug),
+    path('cart/', views.cart),
 ]
--- bookstore/book_store/views.py
+++ bookstore/book_store/views.py
@@ -3,12 +3,16 @@ from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Страница приложения BookStore")
+    return render(request, 'bookstore/index.html')
 
 
 def catalog(request):
-    return HttpResponse("<h1>Каталог</h1>")
+    return render(request, 'bookstore/catalog.html')
 
 
 def catalog_by_slug(request, slug):
     return HttpResponse(f"<h1>Каталог: {slug}</h1>")
+
+
+def cart(request):
+    return render(request, 'bookstore/cart.html')
