629ba6a7d2b549057fc18563757ea8e72dbe9685
AlexProvatorov <sasha.provatorov@gmail.com>
2024-02-05 17:27:05 +0300

    feat: created pagination in index

--- bookstore/book_store/views.py
+++ bookstore/book_store/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 
@@ -7,11 +8,16 @@ from book_store.models import Items
 def index(request):
 
     items = Items.objects.all()
+    paginator = Paginator(items, 6)
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
 
     context = {
         'title': 'Book Store',
         'content': 'Книжный магазин - BOOK STORE',
         'items': items,
+        'page_obj': page_obj,
     }
     return render(request, 'book_store/index.html', context=context)
 
