a1807e3b8ff2371b63004614be2aecca778215d0
AlexProvatorov <sasha.provatorov@gmail.com>
2024-02-20 18:44:04 +0300

    fix/fix view_cart template and add remove_cart_position

--- bookstore/carts/urls.py
+++ bookstore/carts/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', views.view_cart, name='view_cart'),
     path('add_cart/<int:item_id>/', views.add_cart, name='add_cart'),
     path('remove_cart/<int:item_id>/', views.remove_cart, name='remove_cart'),
+    path('remove_cart_position/<int:item_id>/', views.remove_cart_position,
+         name='remove_cart_position'),
 ]
--- bookstore/carts/views.py
+++ bookstore/carts/views.py
@@ -49,20 +49,15 @@ def remove_cart(request, item_id):
     return redirect(request.META['HTTP_REFERER'])
 
 
-def remove_cart(request, item_id):
-
-    if not request.user.is_authenticated:
-        return redirect('login')
+def remove_cart_position(request, item_id):
 
     item_id = get_object_or_404(Item, pk=item_id)
 
-    cart_position, created = Cart.objects.get_or_create(
+    cart_position, deleted = Cart.objects.get_or_create(
         id_customer=request.user,
         id_item=item_id,
     )
 
-    if not created and cart_position.count < cart_position.id_item.count_in_stock:
-        cart_position.count += 1
-        cart_position.save()
+    cart_position.delete()
 
     return redirect(request.META['HTTP_REFERER'])
